d2c23389c3dc5a6fd29a30f5cd511abe11c9a987
eludadev <111eluda111@gmail.com>
2022-06-05 03:42:16 +0100

    Update SEO tags

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,9 +7,7 @@ export default defineNuxtConfig({
 		charset: 'utf-8',
 		viewport: 'width=device-width, initial-scale=1',
 		meta: [
-			{ hid:'description', name:'description', content:'Add this chip input component to your website, at no cost.' },
-		    { hid:'og:image', property: 'og:image', content: '/cover.png' },
-		    { hid:'twitter:image', property: 'twitter:image', content: '/cover.png' }
+			{ hid:'description', name:'description', content:'Add this chip input component to your website, at no cost.' }
 		],
 		link: [
 			{ rel: 'icon', href: '/favicon.ico' }
