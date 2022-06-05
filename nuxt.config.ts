526d30fe4ef559e71f27f305311e0c6960a11e54
eludadev <111eluda111@gmail.com>
2022-06-05 03:45:14 +0100

    Update SEO tags

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,12 +3,14 @@ import { defineNuxtConfig } from 'nuxt'
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
 	head: {
+		meta: [
+			{ hid:'description', name:'description', content:'Add this chip input component to your website, at no cost.' },
+		    { hid:'og:image', property: 'og:image', content: '/cover.png' },
+		    { hid:'twitter:image', property: 'twitter:image', content: '/cover.png' }
+		],
 		title: 'Chippy Demo',
 		charset: 'utf-8',
 		viewport: 'width=device-width, initial-scale=1',
-		meta: [
-			{ hid:'description', name:'description', content:'Add this chip input component to your website, at no cost.' }
-		],
 		link: [
 			{ rel: 'icon', href: '/favicon.ico' }
 		]
